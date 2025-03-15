d8a89ff0a7a273fa8786ae16a6a4a815471480d3
minhieu <tranhieu20034@gmail.com>
2025-03-15 09:39:42 +0700

    Get Users By Id

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -43,8 +43,16 @@ const getCreatePage = (req, res) => {
     res.render("create.ejs");
 };
 
-const getupdatePage = (req, res) => {
-    res.render("edit.ejs");
+const getupdatePage = async (req, res) => {
+    const UserId = req.params.id;
+    let [results, fields] = await connection.query(
+        `SELECT * FROM Users WHERE id = ? `,
+        [UserId]
+    );
+    console.log(">>>Results: ", results);
+    let user = results && results.length > 0 ? results[0] : {};
+    console.log(user);
+    res.render("edit.ejs", { user: user });
 };
 
 module.exports = {
--- src/routes/web.js
+++ src/routes/web.js
@@ -11,7 +11,7 @@ const router = express.Router();
 router.get("/", getHomePage);
 router.get("/abc", getABC);
 router.get("/create", getCreatePage);
-router.get("/update", getupdatePage);
+router.get("/update/:id", getupdatePage);
 router.post("/create-user", postCreateUser);
 
 module.exports = router;
